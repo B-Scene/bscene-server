c2a7a2ed98d91a07b01e31d66d3398a368beb146
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-06-27 16:40:22 +0900

    feat: HTTP 요청이 잘못되었을 때, 전역에서 사용할 예외 코드를 GeneralErrorCode로 작성

---
+++ src/main/java/com/umc/bscene/global/response/code/GeneralErrorCode.java
@@ -0,0 +1,57 @@
+package com.umc.bscene.global.response.code;
+
+import com.umc.bscene.global.constant.StaticValue;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import static com.umc.bscene.global.constant.StaticValue.*;
+
+@Getter
+@RequiredArgsConstructor
+public enum GeneralErrorCode implements BaseResponseCode{
+
+    BAD_REQUEST_ERROR(
+            BAD_REQUEST,
+            "COMMON400_1",
+            "잘못된 요청입니다."),
+    INVALID_HTTP_MESSAGE_BODY(
+            BAD_REQUEST,
+            "COMMON400_2",
+            "HTTP 요청 바디의 형식이 잘못되었습니다."
+    ),
+    INVALID_HTTP_MESSAGE_PARAMETER(
+            BAD_REQUEST,
+            "COMMON400_3",
+            "HTTP 요청 파라미터의 형식이 잘못되었습니다."
+    ),
+    UNAUTHORIZED_ERROR(
+            UNAUTHORIZED,
+            "COMMON401_1",
+            "인증되지 않은 유저는 해당 리소스에 접근할 수 없습니다."
+    ),
+    ACCESS_DENIED_REQUEST(
+            FORBIDDEN,
+            "COMMON403_1",
+            "해당 리소스에 대한 접근 권한이 없습니다."
+    ),
+    NOT_FOUND_ENDPOINT(
+            NOT_FOUND,
+            "COMMON404_1",
+            "존재하지 않는 엔드포인트입니다. 요청 href를 확인해주세요."
+    ),
+    UNSUPPORTED_HTTP_METHOD(
+            METHOD_NOT_ALLOWED,
+            "COMMON405_1",
+            "지원하지 않는 HTTP 메소드입니다."
+    ),
+    SERVER_ERROR(
+            INTERNAL_SERVER_ERROR,
+            "COMMON500_1",
+            "서버 내부에서 알 수 없는 에러가 발생했습니다."
+    )
+    ;
+
+    private final int status;
+    private final String code;
+    private final String message;
+}
