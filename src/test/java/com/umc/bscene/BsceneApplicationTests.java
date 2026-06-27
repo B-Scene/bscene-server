c86247c668b930d740042661239ff87ba7de388c
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-06-27 12:53:46 +0900

    :tada: project init

---
+++ src/test/java/com/umc/bscene/BsceneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.bscene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsceneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
