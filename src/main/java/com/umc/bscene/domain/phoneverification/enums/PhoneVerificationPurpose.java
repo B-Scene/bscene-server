3cf18b8e782487b9be0b233469c1e15155ea8eb0
Yarlang <jmim613@naver.com>
2026-07-03 21:46:51 +0900

    feat: 휴대폰 인증 요청 DTO 및 응답 코드 추가

---
+++ src/main/java/com/umc/bscene/domain/phoneverification/enums/PhoneVerificationPurpose.java
@@ -0,0 +1,7 @@
+package com.umc.bscene.domain.phoneverification.enums;
+
+public enum PhoneVerificationPurpose {
+    SIGNUP,
+    PASSWORD_RESET,
+    FIND_LOGIN_ID
+}
---
+++ src/main/java/com/umc/bscene/domain/phoneverification/response/code/PhoneVerificationErrorCode.java
@@ -0,0 +1,21 @@
+package com.umc.bscene.domain.phoneverification.response.code;
+
+import com.umc.bscene.global.response.code.BaseResponseCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import static com.umc.bscene.global.constant.StaticValue.BAD_REQUEST;
+
+@Getter
+@RequiredArgsConstructor
+public enum PhoneVerificationErrorCode implements BaseResponseCode {
+
+    INVALID_PHONE_VERIFICATION_REQUEST(BAD_REQUEST, "PHONE400_1", "휴대폰 인증 요청 값이 올바르지 않습니다."),
+    VERIFICATION_CODE_MISMATCH(BAD_REQUEST, "PHONE400_2", "인증번호가 올바르지 않습니다."),
+    VERIFICATION_CODE_NOT_FOUND(BAD_REQUEST, "PHONE400_3", "인증번호가 만료되었거나 존재하지 않습니다."),
+    PHONE_VERIFICATION_REQUIRED(BAD_REQUEST, "PHONE400_4", "휴대폰 인증이 필요합니다.");
+
+    private final int status;
+    private final String code;
+    private final String message;
+}
