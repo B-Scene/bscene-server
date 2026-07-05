2be137c2a32e5a188a848bf3ef6a90ceb04c64da
LeeJuYoung <rkfmgm@naver.com>
2026-07-05 22:45:05 +0900

    feat: Band, BandMember repo 생성

---
+++ src/main/java/com/umc/bscene/domain/band/repository/BandRepository.java
@@ -0,0 +1,11 @@
+package com.umc.bscene.domain.band.repository;
+
+import com.umc.bscene.domain.band.entity.Band;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BandRepository extends JpaRepository<Band, Long> {
+
+    List<Band> findByOwner_Id(Long ownerId);
+}
