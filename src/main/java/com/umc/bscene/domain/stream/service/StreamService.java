8514b8f79ccff96d4af409fc4975435c906d59bf
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-06 18:22:17 +0900

    feat: 오디오 스트리밍 인터페이스에 DTO를 import

--- src/main/java/com/umc/bscene/domain/stream/service/StreamService.java
+++ src/main/java/com/umc/bscene/domain/stream/service/StreamService.java
@@ -1,4 +1,23 @@
 package com.umc.bscene.domain.stream.service;
 
+import com.umc.bscene.domain.stream.dto.request.StreamCreateRequest;
+import com.umc.bscene.domain.stream.dto.response.LiveStreamResponse;
+import com.umc.bscene.domain.stream.dto.response.StreamCreateResponse;
+
+import java.util.List;
+import java.util.Set;
+
 public interface StreamService {
+
+    // 인증, 인가
+    Boolean canPublish(String accessToken, String path);
+    Boolean canRead(String accessToken, String path);
+
+    // 방송 시작, 종료
+    StreamCreateResponse createStream(Long userId, StreamCreateRequest request);
+    void closeStream(Long userId, String path);
+
+    // 방송 상태
+    List<LiveStreamResponse> getLiveStreams();
+    void syncLiveState(Set<String> readyPath);
 }
