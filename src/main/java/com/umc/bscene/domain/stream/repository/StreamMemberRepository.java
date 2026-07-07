ca3c7b0fe6e2a74447bc792b780862e6817d18a1
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-07 13:21:28 +0900

    refactor: StreamMemberRepository.existsByIdWithStatuses 메소드에 주석 추가, 매개변수에 @Param 어노테이션 추가

--- src/main/java/com/umc/bscene/domain/stream/repository/StreamMemberRepository.java
+++ src/main/java/com/umc/bscene/domain/stream/repository/StreamMemberRepository.java
@@ -1,12 +1,15 @@
 package com.umc.bscene.domain.stream.repository;
 
 import com.umc.bscene.domain.stream.entity.mapper.StreamMember;
+import com.umc.bscene.domain.stream.enums.StreamMemberStatus;
 import com.umc.bscene.domain.stream.enums.StreamStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StreamMemberRepository extends JpaRepository<StreamMember, Long> {
 
+    // 사용자 ID, 멤버 초대 상태, 방송 상태를 파라미터로 전달하면 레코드의 존재 여부 반환
     @Query("""
 select count(sm) > 0
 from StreamMember sm
@@ -14,5 +17,9 @@ where sm.user.id = :userId
     and sm.status = :memberStatus
     and sm.audioStream.status = :streamStatus
 """)
-    Boolean existsByIdWithStatuses(Long userId, StreamStatus memberStatus, StreamStatus streamStatus);
+    Boolean existsByIdWithStatuses(
+            @Param("userId") Long userId,
+            @Param("memberStatus") StreamMemberStatus memberStatus,
+            @Param("streamStatus") StreamStatus streamStatus
+    );
 }
