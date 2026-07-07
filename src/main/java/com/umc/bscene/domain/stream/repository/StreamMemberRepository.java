fb450da075bc4c90dc32d9352c7d6cef737f96a5
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-07 13:18:52 +0900

    feat: StreamMember에 초대 상태, 라이브 상태, 사용자 ID를 파라미터로 전달하면, 레코드 존재 여부를 반환받는 boolean 타입 메소드 작성

--- src/main/java/com/umc/bscene/domain/stream/repository/StreamMemberRepository.java
+++ src/main/java/com/umc/bscene/domain/stream/repository/StreamMemberRepository.java
@@ -1,7 +1,18 @@
 package com.umc.bscene.domain.stream.repository;
 
 import com.umc.bscene.domain.stream.entity.mapper.StreamMember;
+import com.umc.bscene.domain.stream.enums.StreamStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StreamMemberRepository extends JpaRepository<StreamMember, Long> {
+
+    @Query("""
+select count(sm) > 0
+from StreamMember sm
+where sm.user.id = :userId
+    and sm.status = :memberStatus
+    and sm.audioStream.status = :streamStatus
+""")
+    Boolean existsByIdWithStatuses(Long userId, StreamStatus memberStatus, StreamStatus streamStatus);
 }
