06e2137407a6d01582a383333895aeda7dc476d1
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-08 10:05:23 +0900

    feat: 서비스 구현에 필요한 DTO 객체 필드 상세 구현

--- src/main/java/com/umc/bscene/domain/stream/dto/request/StreamCreateRequest.java
+++ src/main/java/com/umc/bscene/domain/stream/dto/request/StreamCreateRequest.java
@@ -1,4 +1,19 @@
 package com.umc.bscene.domain.stream.dto.request;
 
-public record StreamCreateRequest() {
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record StreamCreateRequest(
+        @NotBlank(message = "title 필드는 필수 값입니다.")
+        String title,
+
+        String description,
+
+        @Future
+        LocalDateTime scheduledAt,
+        List<Long> coHost
+) {
 }
--- src/main/java/com/umc/bscene/domain/stream/dto/response/StreamCreateResponse.java
+++ src/main/java/com/umc/bscene/domain/stream/dto/response/StreamCreateResponse.java
@@ -1,4 +1,8 @@
 package com.umc.bscene.domain.stream.dto.response;
 
-public record StreamCreateResponse() {
+public record StreamCreateResponse(
+        Long audioStreamId,
+        String path,
+        String title
+) {
 }
