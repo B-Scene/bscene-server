730503961bedb9bbcfeb2bfa5021ddf3a01c9232
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-09 12:32:02 +0900

    feat: 현재 라이브 중이 아닌 라이브 접근에 대한 예외 코드 작성

--- src/main/java/com/umc/bscene/domain/stream/enums/code/error/StreamErrorCode.java
+++ src/main/java/com/umc/bscene/domain/stream/enums/code/error/StreamErrorCode.java
@@ -10,6 +10,7 @@ public enum StreamErrorCode implements BaseResponseCode {
 
     FORBIDDEN_REQUEST("STREAM403_1", 403, "해당 리소스에 대한 접근 권한이 없습니다."),
     AUDIO_STREAM_NOT_FOUND("STREAM404_1", 404, "오디오 송출 세션을 찾을 수 없습니다."),
+    AUDIO_STREAM_NOT_LIVE("LIVE404_2", 404, "현재 방송 중인 라이브가 아닙니다."),
     DUPLICATE_LIVE_CREATE_TRY("STREAM409_1", 409, "오디오 송출 세션은 한 유저에 1개만 생성 가능합니다."),
     DB_CONSTRAINTS_FAILED("STREAM409_2", 409, "서버 내에서 라이브 경로 충돌이 발생했습니다.");
 
