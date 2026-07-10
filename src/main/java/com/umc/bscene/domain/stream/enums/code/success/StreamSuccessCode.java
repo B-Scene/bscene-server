7e1216827361888b28162519c44ba9bf212ddc86
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-10 10:35:41 +0900

    feat: 라이브 종료에 성공했을 때 응답에 첨부할 성공 코드 작성

--- src/main/java/com/umc/bscene/domain/stream/enums/code/success/StreamSuccessCode.java
+++ src/main/java/com/umc/bscene/domain/stream/enums/code/success/StreamSuccessCode.java
@@ -14,6 +14,7 @@ public enum StreamSuccessCode implements BaseResponseCode {
     LIVE_ROOM_ENTER_SUCCESS("LIVE200_1", OK, "라이브 방 진입에 성공했습니다."),
     LIVE_ROOM_LEAVE_SUCCESS("LIVE200_2", OK, "라이브 방에서 나갔습니다."),
     ALL_LIVE_SUCCESS("LIVE200_3", OK, "현재 라이브 중인 목록을 조회하는데 성공했습니다."),
+    LIVE_CLOSE_SUCCESS("LIVE200_4", OK, "라이브 종료에 성공했습니다."),
     LIVE_CREATE_SUCCESS("LIVE201_1", CREATED, "라이브 생성에 성공했습니다."),
     ;
 
