56012f4ce09e029ed1725a1a36d5b5d5bdb70c05
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-11 01:25:03 +0900

    fix: 오디오 스트리밍 방을 생성할 때, scheduledAt을 현재와 동일하거나, 과거의 시간대로 설정한 경우에 반환할 예외 응답 메시지를 작성

--- src/main/java/com/umc/bscene/domain/stream/dto/request/StreamCreateRequest.java
+++ src/main/java/com/umc/bscene/domain/stream/dto/request/StreamCreateRequest.java
@@ -14,7 +14,7 @@ public record StreamCreateRequest(
         @Size(max = 100, message = "description 필드는 100자를 초과할 수 없습니다.")
         String description,
 
-        @Future
+        @Future(message = "현재 시각과 일치하거나, 과거인 시간은 예약 시각으로 설정할 수 없습니다.")
         LocalDateTime scheduledAt,
         List<Long> coHost
 ) {
