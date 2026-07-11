f75481027700eaaa3bde246ee5b68eb3a5591c91
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-11 21:28:42 +0900

    feat: 라이브 예약/시작 푸시 알림 발송을 위해, 송출자의 현재 소속 밴드 정보를 받아오는 메소드를 BandMemberPort에 작성

--- src/main/java/com/umc/bscene/domain/stream/port/BandMemberPort.java
+++ src/main/java/com/umc/bscene/domain/stream/port/BandMemberPort.java
@@ -1,8 +1,10 @@
 package com.umc.bscene.domain.stream.port;
 
 import com.umc.bscene.domain.stream.dto.response.BandInfoForGetLiveResponse;
+import com.umc.bscene.domain.stream.dto.response.BandSummaryResponse;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 public interface BandMemberPort {
@@ -13,4 +15,13 @@ public interface BandMemberPort {
      * @return BandInfoForGetLiveResponse를 반환해주세요.
      */
     List<BandInfoForGetLiveResponse> getBandNameWithBandProfileByBroadcasterId(Set<Long> broadcasterIds);
+
+    /**
+     * 라이브 예약/시작 푸시 알림 발송을 위해, 송출자가 활성화된 밴드의 ID와 이름을 전송받기 위한 메소드입니다.
+     * @param broadcasterId 오디오 송출자의 ID를 전달합니다.
+     * @return 송출자의 활성 밴드 BandSummaryResponse를 Optional로 반환해주세요. (활성 밴드 없으면 empty)
+     */
+    Optional<BandSummaryResponse> getBandSummaryByBroadcasterId(Long broadcasterId);
+
+    Optional<BandSummaryResponse> getBandSummaryByBandId(Long bandId);
 }
