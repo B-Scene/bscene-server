3b912c7ef9aaf1581798e502d54413835a46ac27
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-11 21:17:38 +0900

    feat: 예정된 라이브가 아닌데 알림 설정을 하는 경우, 밴드 모드이면서 팬 모드일 때만 접근 가능한 리소스 접근하려고 하는 경우, 밴드 모드에서 라이브 예약을 무한 생성하여 팔로워에게 무한 알림을 발신하는 것을 안티 패턴으로 잡아 예외를 발생하게 하기 위한 코드를 StreamErrorCode에 작성

--- src/main/java/com/umc/bscene/domain/stream/enums/code/error/StreamErrorCode.java
+++ src/main/java/com/umc/bscene/domain/stream/enums/code/error/StreamErrorCode.java
@@ -9,11 +9,14 @@ import lombok.RequiredArgsConstructor;
 public enum StreamErrorCode implements BaseResponseCode {
 
     NO_ACTIVE_BAND_PROFILE("LIVE400_1", 400, "활성화된 밴드 멤버 프로필이 없습니다. 방송에 사용할 밴드 프로필을 먼저 활성화해주세요."),
+    ALARM_TARGET_NOT_SCHEDULED("LIVE400_2", 400, "예정된 라이브가 아니어서 알림을 설정할 수 없습니다."),
     FORBIDDEN_REQUEST("LIVE403_1", 403, "해당 리소스에 대한 접근 권한이 없습니다."),
+    FAN_MODE_ONLY("LIVE403_2", 403, "팬 모드에서만 이용할 수 있는 기능입니다."),
     AUDIO_STREAM_NOT_FOUND("LIVE404_1", 404, "오디오 송출 세션을 찾을 수 없습니다."),
     AUDIO_STREAM_NOT_LIVE("LIVE404_2", 404, "현재 방송 중인 라이브가 아닙니다."),
     ALREADY_LIVE("LIVE409_1", 409, "이미 라이브를 진행중이므로, 새로운 라이브를 시작할 수 없습니다."),
     DB_CONSTRAINTS_FAILED("LIVE409_2", 409, "서버 내에서 라이브 경로 충돌이 발생했습니다."),
+    TOO_MANY_SCHEDULED_LIVES("LIVE409_3", 409, "예약 라이브가 최대 허용 개수를 초과했습니다."),
     ;
 
     private final String code;
