738948c8d3f302f76f97fd469be941ab2f23804e
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-11 20:04:38 +0900

    feat: 다시보기 레코드 조회에 실패한 경우, 저장할 파일이 없는데 저장을 요청한 경우, 방송이 종료되지 않았는데, 종료된 이후의 리소스에 접근하려고 하는 경우, 예외를 던지게끔 StreamErrorCode에 예외 코드 작성

--- src/main/java/com/umc/bscene/domain/stream/enums/code/error/StreamErrorCode.java
+++ src/main/java/com/umc/bscene/domain/stream/enums/code/error/StreamErrorCode.java
@@ -11,8 +11,11 @@ public enum StreamErrorCode implements BaseResponseCode {
     FORBIDDEN_REQUEST("LIVE403_1", 403, "해당 리소스에 대한 접근 권한이 없습니다."),
     AUDIO_STREAM_NOT_FOUND("LIVE404_1", 404, "오디오 송출 세션을 찾을 수 없습니다."),
     AUDIO_STREAM_NOT_LIVE("LIVE404_2", 404, "현재 방송 중인 라이브가 아닙니다."),
+    REPLAY_NOT_FOUND("LIVE404_3", 404, "다시보기를 찾을 수 없습니다."),
+    RECORDING_NOT_FOUND("LIVE404_4", 404, "저장된 녹화 파일이 없습니다."),
     ALREADY_LIVE("LIVE409_1", 409, "이미 라이브를 진행중이므로, 새로운 라이브를 시작할 수 없습니다."),
     DB_CONSTRAINTS_FAILED("LIVE409_2", 409, "서버 내에서 라이브 경로 충돌이 발생했습니다."),
+    STREAM_NOT_CLOSED("LIVE409_3", 409, "아직 방송이 종료되지 않았습니다."),
     ;
 
     private final String code;
