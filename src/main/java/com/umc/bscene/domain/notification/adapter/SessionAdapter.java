f66b9c1ea0590e85671a1ce0bffd08a83e6bc292
Yarlang <jmim613@naver.com>
2026-07-13 14:59:31 +0900

    feat: 세션 알림 Adapter 연결

---
+++ src/main/java/com/umc/bscene/domain/notification/adapter/SessionAdapter.java
@@ -0,0 +1,35 @@
+package com.umc.bscene.domain.notification.adapter;
+
+import com.umc.bscene.domain.notification.exception.NotificationException;
+import com.umc.bscene.domain.session.port.NotifyPort;
+import com.umc.bscene.global.notification.message.PushMessage;
+import com.umc.bscene.global.notification.port.NotificationPort;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class SessionAdapter implements NotifyPort {
+
+    private final NotificationPort notificationPort;
+
+    @Override
+    public void notify(Long receiverId, PushMessage message) {
+        try {
+            notificationPort.send(receiverId, message);
+        } catch (NotificationException exception) {
+            log.warn(
+                    "세션 지원 알림 발송 실패: receiverId={}, code={}",
+                    receiverId,
+                    exception.getBaseResponseCode().getCode(),
+                    exception
+            );
+        } catch (RuntimeException exception) {
+            log.error(
+                    "세션 지원 알림 처리 중 예상하지 못한 오류: receiverId={}",
+                    receiverId,
+                    exception
+            );
+        }
+    }
+}
