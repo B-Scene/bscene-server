a9d72f8247ab2362e2a7e211d67502870e51fc01
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-13 13:20:56 +0900

    feat: LiveMemberProfileResponse를 중첩 클래스로 수정, 최상위 클래스는 LiveMemberProfileResponse 배열 1개만 필드로 갖도록 수정

--- src/main/java/com/umc/bscene/domain/stream/dto/response/LiveMembersResponse.java
+++ src/main/java/com/umc/bscene/domain/stream/dto/response/LiveMembersResponse.java
@@ -9,11 +9,15 @@ import java.util.List;
  * 유저-밴드가 다대다 관계라 밴드마다 다른 예명을 쓸 수 있으므로,
  * 라이브가 생성 시점에 확정한 밴드에 연결된 밴드 멤버 프로필 기준입니다.
  */
-public record LiveMemberProfileResponse(
-        String bandProfileImageUrl,
-        String nickname,
-        String bandName,
-        List<Part> part,
-        Boolean isLeader
+public record LiveMembersResponse(
+        List<LiveMembersResponse> members
 ) {
+    public record LiveMemberProfileResponse(
+            String bandProfileImageUrl,
+            String nickname,
+            String bandName,
+            List<Part> part,
+            Boolean isLeader
+    ) {
+    }
 }
