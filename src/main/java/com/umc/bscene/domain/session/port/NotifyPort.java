43c5d76e18f9c6a57a91d82462dfc5f351cea73f
Yarlang <jmim613@naver.com>
2026-07-17 03:22:41 +0900

    feat: 세션 지원 알림 다중 수신자 발송 구현

--- src/main/java/com/umc/bscene/domain/session/port/NotifyPort.java
+++ src/main/java/com/umc/bscene/domain/session/port/NotifyPort.java
@@ -2,8 +2,10 @@ package com.umc.bscene.domain.session.port;
 
 import com.umc.bscene.global.notification.message.PushMessage;
 
+import java.util.List;
+
 public interface NotifyPort {
 
-    // 세션 도메인에서 사용자에게 알림 발송을 요청
-    void notify(Long receiverId, PushMessage message);
+    // 세션 도메인에서 여러 사용자에게 알림 발송을 요청
+    void notify(List<Long> receiverIds, PushMessage message);
 }
