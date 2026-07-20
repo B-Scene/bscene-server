44f2611b7220adae622a02cb6376e72361ffad30
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-20 20:36:24 +0900

    feat: band.port.PerformancePort에 밴드 ID와 상태를 기입하면, 현재까지 진행한 공연 횟수를 조회할 수 있는 메소드 시그니처를 작성

--- src/main/java/com/umc/bscene/domain/band/port/PerformancePort.java
+++ src/main/java/com/umc/bscene/domain/band/port/PerformancePort.java
@@ -1,5 +1,7 @@
 package com.umc.bscene.domain.band.port;
 
+import com.umc.bscene.domain.performance.enums.PerformanceStatus;
+
 public interface PerformancePort {
     /**
      * 밴드의 공연 수를 조회합니다.
@@ -8,4 +10,13 @@ public interface PerformancePort {
      * @return 해당 밴드의 공연 수
      */
     Long countPerformancesByBandId(Long bandId);
+
+    /**
+     * 밴드가 이제껏 공연한 횟수를 조회합니다.
+     *
+     * @param bandId 공연 횟수를 조회할 밴드 ID
+     * @param status ACTIVE
+     * @return 현재까지 진행한 공연의 수
+     */
+    Long countPerformancesByBandIdAndStatus(Long bandId, PerformanceStatus status);
 }
