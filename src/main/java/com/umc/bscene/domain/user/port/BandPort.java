3d832d042810778537b944a9fa66a1ea3cfaa44b
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-20 22:39:17 +0900

    feat: 나와 연관된 모든 밴드 프로필을 조회하는 메소드를 BandPort에 작성. band.adapter.UserAdapter에 구현 위임

--- src/main/java/com/umc/bscene/domain/user/port/BandPort.java
+++ src/main/java/com/umc/bscene/domain/user/port/BandPort.java
@@ -1,9 +1,12 @@
 package com.umc.bscene.domain.user.port;
 
 import com.umc.bscene.domain.user.dto.response.BandMemberResponse;
+import com.umc.bscene.domain.user.dto.response.MyProfileResponse;
+
+import java.util.List;
 
 public interface BandPort {
 
     BandMemberResponse getActiveBandMemberProfile(Long userId);
-
+    List<MyProfileResponse.MyBandProfile> getAssociatedBandProfiles(Long userId);
 }
