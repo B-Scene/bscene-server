10693be906b442fa639bb25c48986158ee535750
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-20 22:28:02 +0900

    feat: 나와 관련된 프로필들을 목록 조회할 때, 요청 파라미터 값이 잘못되었으면 던질 예외 코드 작성

--- src/main/java/com/umc/bscene/domain/user/response/code/UserErrorCode.java
+++ src/main/java/com/umc/bscene/domain/user/response/code/UserErrorCode.java
@@ -11,7 +11,10 @@ public enum UserErrorCode implements BaseResponseCode {
     FAN_PROFILE_NOT_FOUND(NOT_FOUND, "USER404_1", "팬 프로필을 찾을 수 없습니다."),
     USER_NOT_FOUND(NOT_FOUND, "USER404_2", "사용자를 찾을 수 없습니다."),
     SELF_BLOCK_NOT_ALLOWED(BAD_REQUEST, "USER400_1", "본인은 차단할 수 없습니다."),
-    USER_ALREADY_BLOCKED(CONFLICT, "USER409_1", "이미 차단한 사람입니다.");
+    USER_ALREADY_BLOCKED(CONFLICT, "USER409_1", "이미 차단한 사람입니다."),
+
+    PARAM_BAD_REQUEST(400, "USER400_2",  "잘못된 요청 파라미터 값입니다."),
+    ;
 
     private final int status;
     private final String code;
