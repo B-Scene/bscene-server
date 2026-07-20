bf95b141baf2d75e091f7a7243940deb1b00a56a
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-20 20:21:20 +0900

    fix: Band 패키지에 1번 요청으로 필요한 모든 값을 모두 조회할 수 있게끔 dto 수정

--- src/main/java/com/umc/bscene/domain/user/dto/response/BandMemberResponse.java
+++ src/main/java/com/umc/bscene/domain/user/dto/response/BandMemberResponse.java
@@ -3,6 +3,7 @@ package com.umc.bscene.domain.user.dto.response;
 import java.util.List;
 
 public record BandMemberResponse(
+        Long bandId,
         String profileImageUrl,
         String nickname,
         String bandName,
