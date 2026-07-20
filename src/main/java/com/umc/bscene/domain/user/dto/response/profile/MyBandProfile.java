2b4c0a7d05ec2f28fc94dedc7bf09fc48041ff21
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-20 22:32:30 +0900

    feat: 밴드 프로필 리스트에 사용할 단건 블럭에 필요한 정보 DTO 작성

---
+++ src/main/java/com/umc/bscene/domain/user/dto/response/profile/MyBandProfile.java
@@ -0,0 +1,11 @@
+package com.umc.bscene.domain.user.dto.response.profile;
+
+public record MyBandProfile(
+        Long bandId,
+        String profileImageUrl,
+        String bandName,
+        String genre,       // Genre enum에서 String 변환 필요
+        String region,      // Region enum에서 String 변환 필요
+        Boolean isActive
+) implements MyProfile {
+}
