857ccfffbaf51183957f7fdaad78ea1b7474e12a
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-20 20:47:00 +0900

    feat: 밴드 패키지 내 UserAdapter를 빈으로 등록하는 메소드 작성

--- src/main/java/com/umc/bscene/domain/band/config/BandMemberPortConfig.java
+++ src/main/java/com/umc/bscene/domain/band/config/BandMemberPortConfig.java
@@ -1,13 +1,36 @@
 package com.umc.bscene.domain.band.config;
 
 import com.umc.bscene.domain.band.adapter.StreamAdapter;
+import com.umc.bscene.domain.band.adapter.UserAdapter;
+import com.umc.bscene.domain.band.port.FollowPort;
+import com.umc.bscene.domain.band.port.PerformancePort;
+import com.umc.bscene.domain.band.port.SessionPort;
+import com.umc.bscene.domain.band.repository.BandMemberProfileRepository;
 import com.umc.bscene.domain.band.repository.BandMemberRepository;
 import com.umc.bscene.domain.band.repository.BandRepository;
+import com.umc.bscene.domain.user.port.BandPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BandMemberPortConfig {
+
+    @Bean
+    public BandPort BandUserAdapter(
+            BandMemberProfileRepository bandMemberProfileRepository,
+            BandMemberRepository bandMemberRepository,
+            FollowPort followPort,
+            SessionPort sessionPort,
+            PerformancePort performancePort
+    ) {
+        return new UserAdapter(
+                bandMemberProfileRepository,
+                bandMemberRepository,
+                followPort,
+                sessionPort,
+                performancePort
+        );
+    }
 
     @Bean
     public StreamAdapter streamAdapter(BandMemberRepository bandMemberRepository, BandRepository bandRepository) {
