fabb2727e4f0e30fac191ed43d03c80ea649851a
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-20 20:27:38 +0900

    feat: SessionConfig에 밴드쪽과 연결된 어댑터를 빈으로 등록하는 메소드 작성

--- src/main/java/com/umc/bscene/domain/session/config/SessionConfig.java
+++ src/main/java/com/umc/bscene/domain/session/config/SessionConfig.java
@@ -1,5 +1,8 @@
 package com.umc.bscene.domain.session.config;
 
+import com.umc.bscene.domain.band.port.SessionPort;
+import com.umc.bscene.domain.session.adapter.BandAdapter;
+import com.umc.bscene.domain.session.repository.SessionApplicationSubmissionRepository;
 import com.umc.bscene.domain.session.scheduler.SessionRecruitmentReminderScheduler;
 import com.umc.bscene.domain.session.service.SessionRecruitmentReminderService;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +10,11 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SessionConfig {
+
+    @Bean
+    public SessionPort SessionBandAdapter(SessionApplicationSubmissionRepository sessionApplicationSubmissionRepository) {
+        return new BandAdapter(sessionApplicationSubmissionRepository);
+    }
 
     // 세션 모집 마감 24시간 전 알림 발송 스케줄러
     @Bean
