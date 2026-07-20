2dfa593b30c6d3bf7cb327dfccb63f7dc1181e7b
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-20 22:59:04 +0900

    fix: dto prjection 사용 시 enum이 자동으로 String으로 매핑되지 않아 enum 타입을 의존하도록 수정

--- src/main/java/com/umc/bscene/domain/user/dto/response/MyBandProfile.java
+++ src/main/java/com/umc/bscene/domain/user/dto/response/MyBandProfile.java
@@ -1,10 +1,13 @@
 package com.umc.bscene.domain.user.dto.response;
 
+import com.umc.bscene.domain.auth.enums.onboarding.Genre;
+import com.umc.bscene.domain.auth.enums.onboarding.Region;
+
 public record MyBandProfile(
         Long bandId,
         String profileImageUrl,
         String bandName,
-        String genre,       // Genre enum에서 String 변환 필요
-        String region,      // Region enum에서 String 변환 필요
+        Genre genre,       // Genre enum에서 String 변환 필요
+        Region region,      // Region enum에서 String 변환 필요
         Boolean isActive
 ){ }
