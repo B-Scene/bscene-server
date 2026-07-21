d13ea89926c931d978340f1a93b35335a76630f3
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-21 21:16:43 +0900

    feat: auth 패키지 빈등록 설정 파일에 auth.adapter.UserAdapter를 빈으로 등록하는 메소드 작성

--- src/main/java/com/umc/bscene/domain/auth/config/AuthConfig.java
+++ src/main/java/com/umc/bscene/domain/auth/config/AuthConfig.java
@@ -1,7 +1,11 @@
 package com.umc.bscene.domain.auth.config;
 
 import com.umc.bscene.domain.auth.adapter.StreamAdapter;
+import com.umc.bscene.domain.auth.adapter.UserAdapter;
+import com.umc.bscene.domain.auth.repository.credential.LocalCredentialRepository;
 import com.umc.bscene.domain.auth.repository.term.UserTermsRepository;
+import com.umc.bscene.domain.oauth.repository.OauthAccountRepository;
+import com.umc.bscene.domain.user.port.AuthPort;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +20,17 @@ public class AuthConfig {
         return new StreamAdapter(
                 userTermsRepository,
                 liveNotificationTermsId
+        );
+    }
+
+    @Bean
+    public AuthPort authPort(
+            LocalCredentialRepository localCredentialRepository,
+            OauthAccountRepository oauthAccountRepository
+    ) {
+        return new UserAdapter(
+                localCredentialRepository,
+                oauthAccountRepository
         );
     }
 }
