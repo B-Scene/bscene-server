82f258682910172a262b8d63e7c3fba363385118
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-22 12:38:39 +0900

    fix: requestId에서 조금 더 명확한 profileId임을 받고, 해당 프로필의 타입 역시 같이 받도록 수정

--- src/main/java/com/umc/bscene/domain/user/dto/request/UserModeUpdateRequest.java
+++ src/main/java/com/umc/bscene/domain/user/dto/request/UserModeUpdateRequest.java
@@ -1,11 +1,13 @@
 package com.umc.bscene.domain.user.dto.request;
 
+import com.umc.bscene.domain.user.enums.UserMode;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record UserModeUpdateRequest(
         @NotNull(message = "모드 변경 요청에는 반드시 id가 필요합니다.")
         @Min(value = 1, message = "유효하지 않은 id입니다.")
-        Long requestId
+        Long profileId,
+        UserMode type
 ) {
 }
