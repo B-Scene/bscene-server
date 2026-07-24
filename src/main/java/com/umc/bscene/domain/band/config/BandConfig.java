b2be4852b8e81bd658f0dcf5edb84659d395fade
Yarlang <jmim613@naver.com>
2026-07-24 20:53:30 +0900

    chore: 밴드 초대 알림 조회 어댑터 빈 등록

--- src/main/java/com/umc/bscene/domain/band/config/BandConfig.java
+++ src/main/java/com/umc/bscene/domain/band/config/BandConfig.java
@@ -1,16 +1,14 @@
 package com.umc.bscene.domain.band.config;
 
+import com.umc.bscene.domain.band.adapter.BandInviteNotificationAdapter;
 import com.umc.bscene.domain.band.adapter.FanHomeAdapter;
 import com.umc.bscene.domain.band.adapter.SearchAdapter;
 import com.umc.bscene.domain.band.adapter.SessionAdapter;
-import com.umc.bscene.domain.band.port.StreamPort;
 import com.umc.bscene.domain.band.repository.BandMemberRepository;
 import com.umc.bscene.domain.band.repository.BandRepository;
 import com.umc.bscene.domain.band.service.BandRecommendationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Optional;
 
 @Configuration
 public class BandConfig {
@@ -30,5 +28,11 @@ public class BandConfig {
     @Bean
     public SessionAdapter sessionBandMemberAdapter(BandMemberRepository bandMemberRepository) {
         return new SessionAdapter(bandMemberRepository);
+    }
+
+    // 알림 도메인 BandInvitePort 구현 어댑터
+    @Bean
+    public BandInviteNotificationAdapter bandInviteNotificationAdapter(BandMemberRepository bandMemberRepository) {
+        return new BandInviteNotificationAdapter(bandMemberRepository);
     }
 }
