120d299674f82761473ff6de0996d3b67374da18
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-08-06 18:27:05 +0900

    fix: stream.adapter.BandAdapter.findOpenLiveId에서 liveId를 찾지 못했을 때 예외를 던지는 것을 Optional로 래핑하여 안전하게 null을 반환하도록 수정

--- src/main/java/com/umc/bscene/domain/band/service/BandService.java
+++ src/main/java/com/umc/bscene/domain/band/service/BandService.java
@@ -34,6 +34,7 @@ import com.umc.bscene.domain.band.repository.BandRepository;
 import com.umc.bscene.domain.band.repository.MusicLinkRepository;
 import com.umc.bscene.domain.band.response.code.BandErrorCode;
 import com.umc.bscene.domain.search.event.BandChangedEvent;
+import com.umc.bscene.domain.stream.exception.StreamException;
 import com.umc.bscene.domain.user.entity.User;
 import com.umc.bscene.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -119,8 +120,13 @@ public class BandService {
         Band band = getBand(bandId);
 
         Long followerCount = followPort.countFollowersByBandId(bandId);
+        Long liveId;
         boolean isFollowing = followPort.isFollowing(userId, bandId);
-        Long liveId = streamPort.findOpenLiveId(bandId).orElse(null);
+        try {
+            liveId = streamPort.findOpenLiveId(bandId).orElse(null);
+        } catch (StreamException e) {
+            liveId = null;
+        }
 
         return BandDetailResponse.of(band, followerCount, isFollowing, liveId);
     }
